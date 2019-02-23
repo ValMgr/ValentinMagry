bff1b711d8a2e07f1fe0cf622d65e3cf1496b5ac
V.Mgr <ValMgr@users.noreply.github.com>
2019-02-23 20:05:31 +0100

    Transition works

--- Assets/main.js
+++ Assets/main.js
@@ -1,3 +1,6 @@
+/* Valentin Magry - MMI Bordeaux Montaigne Student - 2019 */
+/* Using Barba.js http://barbajs.org/ */
+
 Barba.Pjax.start();
 
 /* Transition From Index to Contacts */
@@ -14,66 +17,73 @@ var FromItoC = Barba.BaseTransition.extend({
   },
 
   fadeOut: function() {
-    this.oldContainer.classList.add("slide-left");
+    this.oldContainer.classList.add("slide-ToLeft");
 
     this.oldContainer.addEventListener('animationstart', function(){
-      this.oldContainer.classList.remove("slide-left");
+      this.oldContainer.classList.remove("slide-ToLeft");
       this.done();
     });
   },
 
   fadeIn: function() {
-    this.newContainer.classList.add("slide-right");
+    this.newContainer.classList.add("slide-FromRight");
 
     var that = this;
 
     this.newContainer.addEventListener('animationend', function(){
-      that.newContainer.classList.remove("slide-right");
+      that.newContainer.classList.remove("slide-FromRight");
       that.done();
     });
-  }
+  },
+
+
+
+
 });
 
 
 /* Transition From Contacts to Index */
 var FromCtoI = Barba.BaseTransition.extend({
+
   start: function() {
-    Promise.all([this.newContainerLoading, this.fadeOut()]).then(this.fadeIn.bind(this));
+  Promise.all([this.newContainerLoading, this.fadeOut()]).then(this.fadeIn.bind(this));
   },
 
   fadeOut: function() {
-    this.oldContainer.classList.add("slide-right");
+    this.oldContainer.classList.add("slide-ToRight");
 
     this.oldContainer.addEventListener('animationstart', function(){
-      this.oldContainer.classList.remove("slide-right");
+      this.oldContainer.classList.remove("slide-ToRight");
       this.done();
     });
   },
 
   fadeIn: function() {
-    this.newContainer.classList.add("slide-left");
+    this.newContainer.classList.add("slide-FromLeft");
 
     var that = this;
 
     this.newContainer.addEventListener('animationend', function(){
-      that.newContainer.classList.remove("slide-left");
+      that.newContainer.classList.remove("slide-FromLeft");
       that.done();
     });
   },
 
+
+  
+
 });
+
+
+ 
+
 
 
 /* Appel des transitions en fonctions des pages actuels et cibles */
 
 Barba.Pjax.getTransition = function() {
-  
-  
-  if()
-  
-  
-
-
+   
+return FromCtoI;
   
 };
 
