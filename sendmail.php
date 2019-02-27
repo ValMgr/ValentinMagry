cc7b3a242d12acf5d13213528040dbb404658876
V.Mgr <ValMgr@users.noreply.github.com>
2019-02-27 01:30:22 +0100

    v5.2

---
+++ sendmail.php
@@ -0,0 +1,33 @@
+<!-- Valentin Magry - MMI Bordeaux Montaigne Student - 2019 -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Valentin Magry | Work In Progress</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <script src="main.js"></script>
+</head>
+<body>
+
+<div class="bodypage">
+</div>
+
+
+<?php 
+
+
+
+
+
+
+
+
+
+
+?>
+    
+</body>
+</html>
