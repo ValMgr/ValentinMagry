2e1a2ebeade03b09c3ed9920edbc8afb837fdfba
V.Mgr <ValMgr@users.noreply.github.com>
2019-04-15 11:23:06 +0200

    Ahhh

--- sendmail.php
+++ sendmail.php
@@ -18,53 +18,13 @@
 </head>
 <body> 
 
-        <div id="barba-wrapper">
-            <div class="barba-container" data-namespace="contact">
-                <div class="bodypage">
+       <?php 
 
-                        <div class="title-contact">
-                                <h3 id="contact-title">CONTACT</h3>
-                                <h3>ME</h3>
-                        </div>
-    
-                        <div class="Links" id="Contact-GoBack">
-                            <div class="linke">
-                                <a href="index.html">GO BACK</a>
-                                <span class="links_underlined"></span>
-                            </div>
-                        </div>
+       $to = "val.magry@gmail.com";
+       $title = "Mail send from Valentin-magry.fr";
+       $message = "test";
 
-                        <div class="contact-content">
-                            <form action="sendmail.php" method="post" id="form">
-                                <div class="Name" id="N1">
-                                    <label for="firstname" class="label">First Name</label>
-                                    <input type="text" id="firstname" name="firstname">
-                                </div>
-                                <div class="Name" id="N2">
-                                    <label for="lastname" class="label">Last Name</label>
-                                    <input type="text" id="lastname" name="lastname">
-                                </div>
-                                <div class="email">
-                                    <label for="email" class="label">E-mail</label>
-                                    <input type="text" id="email" name="email">
-                                </div>
-                                <div class="email">
-                                    <label for="email-content" class="label">Message</label>
-                                    <textarea type="text" id="email-content" name="email-content"></textarea>
-                                </div>
-                                <input type="submit" value="SUBMIT" id="submit">
-                            </form>
-                        </div>
-
-
-
-                </div>
-            </div>
-        </div>
-
-        <!-- JavaScript Links -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/barba.js/1.0.0/barba.min.js"></script>
-        <script src="Assets/main.js"></script>
+       mail($to,$title,$message);
 
 </body>
 </html>
